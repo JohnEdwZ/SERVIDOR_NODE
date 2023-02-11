4d8de84a5709401e93c0589b5e15e97d42534f3c
John Edward <edward.rodriguez.v@live.com.mx>
2023-02-10 19:39:15 -0500

    multer

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
-const express = require("express")
+const express = require("express");
+const multer = require("multer");
+
+const upload = multer({dest: 'uploads/'})
 const app = express();
 
+app.use(express.json())
+
 app.get('/', function(req, res){
-    res.send("Hola Mundo")
+    res.send('Hola Mundo')
 })
+
+app.post('/imagen', upload.single('imagen'), function(req, res){
+
+    const body = req.body
+    const imagen = req.file
+    
+
+    console.log(imagen)
+
+    res.send("Hola mundo POST")
+})
+
 app.listen(4000)
